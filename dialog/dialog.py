13c6d8ddd3c64c7e4c85287ef112b6b7a05f76c3
lanceyu120@gmail.com <lanceyu120@gmail.com>
2020-07-10 15:42:24 +0800

    RPG Game

--- dialog/dialog.py
+++ dialog/dialog.py
@@ -10,27 +10,28 @@ from dialog import blit_text
 
 
 class Dialog:
-    def __init__(self, width, height, text, npc=False):
+    def __init__(self, width, height, text, loop=False):
         header_h = 0
         header_w = 0
-        if npc:
+        if loop:
             img_path = os.path.join('resources', 'img', 'npc', '00000.png')
             temp_header = pygame.image.load(img_path)
+            temp_header.set_colorkey((255, 255, 255))
             header_w = temp_header.get_width()//2
             header_h = temp_header.get_height()//2
             header = pygame.transform.scale(temp_header, (header_w, header_h))
-        dialog_path = os.path.join('resources', 'img', 'dialog', 'dialog.png')
+        dialog_path = os.path.join('resources', 'img', 'dialog', 'dialog2.png')
         temp_dialog = pygame.image.load(dialog_path)
         dialog_w = width
         dialog_h = height
         dialog = pygame.transform.scale(temp_dialog, (dialog_w, dialog_h))
         font_path = os.path.join('resources', 'font', 'msyh.ttf')
         font = pygame.font.Font(font_path, 18)
-        blit_text(dialog, text, (20, 25), font, (0, 0, 1))
+        blit_text(dialog, text, (40, 30), font, (255, 255, 254))
         h = max(header_h, dialog_h)
         w = header_w + dialog_w
         self.surface = pygame.Surface((w, h))
-        self.surface.set_colorkey((0, 0, 0))
-        if npc:
+        self.surface.set_colorkey((255, 255, 255))
+        if loop:
             self.surface.blit(header, (0, 0))
         self.surface.blit(dialog, (header_w, 0))
